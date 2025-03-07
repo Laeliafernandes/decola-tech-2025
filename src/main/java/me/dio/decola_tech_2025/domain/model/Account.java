da2397196596216c3d9e232d930084a8cf9ca762
laeliafernandes <laelialfernandes@gmail.com>
2025-03-07 01:06:19 -0300

    Implementação da camada de domínio.

---
+++ src/main/java/me/dio/decola_tech_2025/domain/model/Account.java
@@ -0,0 +1,55 @@
+package me.dio.decola_tech_2025.domain.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "tb_account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    private String agency;
+
+    @Column(precision = 13, scale = 2)
+    private String balance;
+
+    @Column(name = "additional_limit", precision = 13, scale = 2)
+    private BigDecimal limit;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    public String getAgency() {
+        return agency;
+    }
+    public void setAgency(String agency) {
+        this.agency = agency;
+    }
+    public String getBalance() {
+        return balance;
+    }
+    public void setBalance(String balance) {
+        this.balance = balance;
+    }
+    public BigDecimal getLimit() {
+        return limit;
+    }
+    public void setLimit(BigDecimal limit) {
+        this.limit = limit;
+    }
+}
---
+++ src/main/java/me/dio/decola_tech_2025/domain/model/News.java
@@ -0,0 +1,7 @@
+package me.dio.decola_tech_2025.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tb_news")
+public class News extends BaseId {
+}
